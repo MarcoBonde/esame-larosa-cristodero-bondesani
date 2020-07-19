86e7fb566f39aeae5be72967741ed5e908e6f8cd
MarcoBondesaniNaba <MarcoBondeNaba@users.noreply.github.com>
2020-07-19 16:43:13 +0200

    Patch per far funzionare tutto

---
+++ Assets/script/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{// Stores a reference of the Rigidbody2D component (if any)
+    Rigidbody2D _rigidbody;
+    public float jumpMultiplier = 10;
+    bool _isJumping;
+    
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        
+    }
+private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !_isJumping)
+        {
+            _isJumping = true;
+
+            Vector2 jumpForce = Vector2.up * jumpMultiplier;
+
+            _rigidbody.AddForce(jumpForce, ForceMode2D.Impulse);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Platform"))
+        {
+            _isJumping = false;
+        }
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+ 
+}
---
+++ Assets/script/ShootSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootSystem : MonoBehaviour
+{ public Transform prefab;
+    public Rigidbody rb;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("fire"))
+        {
+            Instantiate(prefab);
+            rb.velocity = new Vector3(10, 0, 0);
+        }
+    }
+}
