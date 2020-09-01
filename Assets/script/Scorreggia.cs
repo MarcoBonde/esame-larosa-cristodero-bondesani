d5a226d772e813a6fbc1214db994b0429839c329
michaellaros <56259544+michaellaros@users.noreply.github.com>
2020-09-01 18:21:49 +0200

    score

---
+++ Assets/script/Scorreggia.cs
@@ -0,0 +1,31 @@
+﻿
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scorreggia : MonoBehaviour //easter egg
+{
+    public Transform player;
+    public Text scoreText;
+    private float timer;
+    private int score;
+
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+
+        if (timer > 1f)
+        {
+
+            score += 1;
+
+            //We only need to update the text if the score changed.
+            scoreText.text = score.ToString();
+
+            //Reset the timer to 0.
+            timer = 0;
+        }
+    }
+}
+
