33ee90c95234428071cfc27b86ae879a11a0b23b
michaellaros <56259544+michaellaros@users.noreply.github.com>
2020-09-11 16:01:28 +0200

    enemy shoot

--- Assets/script/script enemy/enemybulletinteraction.cs
+++ Assets/script/script enemy/enemybulletinteraction.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemybulletinteraction : MonoBehaviour
+public class Enemybulletinteraction : MonoBehaviour
 {
     Rigidbody2D _rigidbody;
 
