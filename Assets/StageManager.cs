f788a8af58d95eeeb4b4b963788b27ce17db7d92
MarcoBondesaniNaba <MarcoBondeNaba@users.noreply.github.com>
2020-09-14 17:21:28 +0200

    animazioni sample

---
+++ Assets/StageManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageManager : MonoBehaviour
+{
+    public GameObject stage1;
+    public GameObject stage2;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
