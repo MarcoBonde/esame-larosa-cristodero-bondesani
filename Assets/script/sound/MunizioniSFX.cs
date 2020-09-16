c150bd7023814485b390b7d8f1fbb13207698aa9
ElisaCristoderoStud <elisa_cristodero@studenti.naba.it>
2020-09-16 15:35:45 +0200

    sound

---
+++ Assets/script/sound/MunizioniSFX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MunizioniSFX : MonoBehaviour
+
+{
+
+    public AudioSource MunizioniSound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        MunizioniSound = GetComponent <AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            MunizioniSound.Play();
+        }
+
+
+    }
+}
